94a6943b98162e6daf8736326c5f7585efe2299f
Amit Rajput <amitrajput98267313@gmail.com>
2025-10-12 23:19:25 +0530

    making seperate components

--- react-re/Bootstrap/2-Todo-app/src/App.jsx
+++ react-re/Bootstrap/2-Todo-app/src/App.jsx
@@ -1,50 +1,16 @@
 import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem1";
 
 function App() {
   return (
-    <center className="todo-container">
-      <AppName/>
-      <div class="container">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter Todo Here" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-         
-        </div><div class="row">
-          <div class="col-6">
-            Buy milk
-          </div>
-          <div class="col-4">
-            4/10/25
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>  </div><div class="row">
-          <div class="col-6">
-            Go to collage
-          </div>
-          <div class="col-4">
-            6/10/25
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-         
-        </div>
-      </div> 
-    </center>
-  )
+    <div class="container text center">
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
+      <TodoItem2 />
+    </div>
+  );
 }
 export default App;
---
+++ react-re/Bootstrap/2-Todo-app/src/components/TodoItem1.jsx
@@ -0,0 +1,17 @@
+function TodoItem1() {
+  return (
+    <div class="container text center">
+      <div class="row">
+        <div class="col-6">Buy milk</div>
+        <div class="col-4">4/10/25</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
