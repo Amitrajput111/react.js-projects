53aa0c8a91701922b3c57b936799a950281c3318
Amit Rajput <amitrajput98267313@gmail.com>
2025-10-13 22:45:49 +0530

    adding css in all elements

--- react-re/Bootstrap/2-Todo-app/src/App.jsx
+++ react-re/Bootstrap/2-Todo-app/src/App.jsx
@@ -2,14 +2,17 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem1 from "./components/TodoItem1";
 import TodoItem2 from "./components/TodoItem1";
+import "./App.css";
 
 function App() {
   return (
     <div class="container text center">
       <AppName />
       <AddTodo />
-      <TodoItem1 />
-      <TodoItem2 />
+      <div className="items-container">
+        <TodoItem1 />
+        <TodoItem2 />
+      </div>
     </div>
   );
 }
--- react-re/Bootstrap/2-Todo-app/src/components/TodoItem2.jsx
+++ react-re/Bootstrap/2-Todo-app/src/components/TodoItem2.jsx
@@ -1,11 +1,14 @@
 function TodoItem2() {
+    let todoName = "Buy Milk";
+  let todoDate = "6/10/25";
+
   return (
-    <div class="container text center">
-      <div class="row">
-        <div class="col-6">Go to cllg </div>
-        <div class="col-4">6/10/25</div>
+    <div class="container ">
+      <div class="row kg row">
+        <div class="col-6">{todoName} </div>
+        <div class="col-4">{todoDate}</div>
         <div class="col-2">
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger kg-button">
             Delete
           </button>
         </div>
